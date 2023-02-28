0e2281be5273372df4090393a018eef19fd3cf83
PrimePlatypus <primeplatypus@amogus.local>
2023-02-27 19:58:01 -0500

    actually use the correct workflow file

--- .github/workflows/mac-post.sh
+++ .github/workflows/mac-post.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env zsh
 
 set -e
-export WORKFLOW_ROOT=/Users/runner/work/futurerestore/futurerestore/.github/workflows
-export DEP_ROOT=/Users/runner/work/futurerestore/futurerestore/dep_root
-export BASE=/Users/runner/work/futurerestore/futurerestore/
+export BASE=${TMPDIR}/Builder/repos/futurerestore/.github/workflows
 
-cd ${BASE}
+cd ${BASE}/../../
 export FUTURERESTORE_VERSION=$(git rev-list --count HEAD | tr -d '\n')
-export FUTURERESTORE_VERSION_SHA=$(git rev-parse HEAD | tr -d '\n')
 export FUTURERESTORE_VERSION_RELEASE=$(cat version.txt | tr -d '\n')
-echo -n $FUTURERESTORE_VERSION_SHA > latest_build_sha.txt
-echo -n $FUTURERESTORE_VERSION > latest_build_num.txt
-lipo -create -arch x86_64 cmake-build-release-x86_64/src/futurerestore -arch arm64 cmake-build-release-arm64/src/futurerestore -output futurerestore
-codesign --force --sign - futurerestore
-tar cpPJf "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-RELEASE.tar.xz" futurerestore
-rm -rf futurerestore
-lipo -create -arch x86_64 cmake-build-debug-x86_64/src/futurerestore -arch arm64 cmake-build-debug-arm64/src/futurerestore -output futurerestore
-codesign --force --sign - futurerestore
-tar cpPJf "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-DEBUG.tar.xz" futurerestore
-rm -rf futurerestore
-lipo -create -arch x86_64 cmake-build-asan-x86_64/src/futurerestore -arch arm64 cmake-build-asan-arm64/src/futurerestore -output futurerestore
-codesign --force --sign - futurerestore
-tar cpPJf "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-ASAN.tar.xz" futurerestore
+cd ${BASE}
+cp -RpP "${TMPDIR}/Builder/macOS_x86_64_1300_Release/bin/futurerestore" futurerestore
+tar cpPJf "futurerestore-macOS-x86_64-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-RELEASE.tar.xz" futurerestore
+cp -RpP "${TMPDIR}/Builder/macOS_x86_64_1300_Debug/bin/futurerestore" futurerestore
+tar cpPJf "futurerestore-macOS-x86_64-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-DEBUG.tar.xz" futurerestore
+cp -RpP "${TMPDIR}/Builder/macOS_arm64_1700_Release/bin/futurerestore" futurerestore
+tar cpPJf "futurerestore-macOS-arm64-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-RELEASE.tar.xz" futurerestore
+cp -RpP "${TMPDIR}/Builder/macOS_arm64_1700_Debug/bin/futurerestore" futurerestore
+tar cpPJf "futurerestore-macOS-arm64-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-DEBUG.tar.xz" futurerestore
